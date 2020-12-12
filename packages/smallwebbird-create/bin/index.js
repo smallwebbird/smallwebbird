acbc6ada52b64e30c8bcce6e4c6c1052d4983c0d
smallwebbird <1175543548@qq.com>
2020-12-12 14:06:13 +0800

    @smallwebbird/create包增加构建初始化目录功能

--- packages/smallwebbird-create/bin/index.js
+++ packages/smallwebbird-create/bin/index.js
@@ -1,11 +1,69 @@
 #!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
+const { program } = require('commander');
+const figlet = require('figlet');
+
+const packageJSON = require('../package.json');
+
 
 const cwd = process.cwd();
 
-fs.mkdir(path.join(cwd, './abc'), {}, (err) => {
-  if (err) {
-    console.log('创建文件夹成功');
+async function run() {
+  const projectName = await runCommand();
+  await createInitDir(projectName);
+
+}
+
+function createInitDir (projectName) {
+  return new Promise ((r, j) => {
+    if (projectName) {
+      fs.mkdir(path.join(cwd, `../${String(projectName)}`), {}, (err) => {
+        if (err) {
+            j(err);
+        } else {
+          r();
+          console.log('初始化目录成功！！！');
+        }
+      });
+    }
+  });
+}
+
+async function runCommand () {
+  let projectName = await getProjectName();
+  if (!projectName) {
+    console.log(figlet.textSync('smallwebbird', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 200,
+      whitespaceBreak: true
+    }));
+    process.exit(1);
   }
-});
+  return projectName;
+}
+
+async function getProjectName () {
+  const version = packageJSON.version;
+  return new Promise((r, j) => {
+    program
+    .version(version)
+    .arguments('[project-name]')
+    .usage('输入目录')
+    .action((projectName) => {
+      r(projectName);
+    });
+    program.parse(process.argv);
+  });
+}
+
+run()
+  .then(() => {
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
